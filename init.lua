28da2df44151f574bebce9e7d3df02eb340bb91c
Rene Wuensche <rene-666@gmx.de>
2025-12-12 13:48:12 +0100

    add jj and jk keybinds

--- init.lua
+++ init.lua
@@ -169,6 +169,12 @@ vim.o.confirm = true
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- Esc with jj
+vim.keymap.set('i', 'jj', '<Esc>')
+
+-- Esc and save with jk
+vim.keymap.set('i', 'jk', '<Esc>:w<CR>')
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
